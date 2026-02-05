232a38e90e3453aad8a52d8cc9620ab28fcaa8b6
suryamondal22 <suryamondal768@gmail.com>
2026-02-05 12:01:32 +0530

    Refactor appwrite service and PostForm component by removing commented-out code for improved readability and maintainability

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,34 +1,3 @@
-// import Select from "./Select";
-// import Header from "./Header/header";
-// import Footer from "./Footer/Footer";
-// import Container from "./Container/conainer";
-// import Logo from "./Logo";
-// import LogoutBtn from "./Header/LogoutBtn";
-// import RTE from "./RTE";
-// import Signup from "./Signup";
-// import Login from "./Login";
-// import Button from "./Button";
-// import PostForm from "./Post-form/PostFrom";
-// import PostCard from "./PostCard";
-// import AuthLayout from "./AuthLayout";
-// import Input from "./Input";
-// export {
-//     Header,
-//     Footer,
-//     Container,
-//     Logo,
-//     LogoutBtn,
-//     RTE,
-//     Signup,
-//     Login,
-//     Button,
-//     PostForm,
-//     PostCard,
-//     AuthLayout, 
-//     Input,
-//     Select,
-// }
-
 import Select from "./Select";
 import Header from "./Header/header";
 import Footer from "./Footer/Footer";
